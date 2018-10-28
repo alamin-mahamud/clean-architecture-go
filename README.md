c822b4ac5995382e71e6d290bc290814314a3c55
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-10-28 08:02:00 +0600

    :building_construction: domain or entities are added.

---
+++ README.md
@@ -0,0 +1,21 @@
+* Domain:
+** Customer Entity
+** Item Entity
+** Order Entity
+
+* Use Cases:
+** User entity
+** Use case: Add item to Order
+** Use case: Get items in Order
+** Use case: Admin adds item to the Order
+
+* Interfaces:
+** Web services for Item/Order handling
+** Repositories for use cases and domain entities persistence
+
+* Infrastracture
+** The Database
+** Code that handles DB Connection
+** Go Standard Library
+
+(Link)[https://manuel.kiessling.net/2012/09/28/applying-the-clean-architecture-to-go-applications/]
---
+++ domain/domain.go
@@ -0,0 +1,58 @@
+package domain
+
+import "errors"
+
+type Customer struct {
+	Id   int
+	Name string
+}
+
+type Item struct {
+	Id        int
+	Name      string
+	Value     float64
+	Available bool
+}
+
+type Order struct {
+	Id       int
+	Customer Customer
+	Items    []Item
+}
+
+type CustomerRepository interface {
+	Store(customer Customer)
+	FindById(id int) Customer
+}
+
+type ItemRepository interface {
+	Store(item Item)
+	FindById(id int) Item
+}
+
+type OrderRepository interface {
+	Store(order Order)
+	FindById(id int) Order
+}
+
+func (order *Order) Add(item Item) error {
+	if !item.Available {
+		return errors.New("Cannot add unavailable items to orders.")
+	}
+
+	if order.value()+item.Value > 250.00 {
+		return errors.New(`An order may not exceed a total value of $250.00`)
+	}
+
+	order.Items = append(order.Items, item)
+	return nil
+}
+
+func (order *Order) value() float64 {
+	sum := 0.0
+	for i := range order.Items {
+		// sum += order.Items[i].Value
+		sum += i.Value
+	}
+	return sum
+}
